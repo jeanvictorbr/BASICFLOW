e4fd54622605ddf95dcc69eafa6a4ba3e8173d7c
jeanvictorbr <jeanvictorbr@gmail.com>
2025-10-02 10:50:37 -0300

    Update configurar.js

--- commands/configurar.js
+++ commands/configurar.js
@@ -1,13 +1,20 @@
 // Ficheiro: commands/configurar.js
+// Comando simplificado que chama o handler dedicado.
+
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { handleConfigCommand } = require('../handlers/config_handler.js');
+
+// CORREÇÃO: O caminho para o handler foi corrigido de '../' para './'
+// Isto diz ao ficheiro para procurar a pasta 'handlers' dentro da pasta 'commands'.
+const { handleConfigCommand } = require('./handlers/config_handler.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('configurar')
         .setDescription('Abre o painel de configuração do BasicFlow.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+        
     async execute(interaction) {
+        // A única responsabilidade deste ficheiro é chamar o handler robusto.
         await handleConfigCommand(interaction);
     },
 };
